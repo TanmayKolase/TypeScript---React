eb51ffbbb4ec22b1651e1ea0e5eef4bebac49f5b
TanmayKolase <tanmay.kolase@p99soft.com>
2024-10-25 18:54:47 +0530

    Create new CourseGoal component with initial code

---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const CourseGoal = () => {
+  return (
+    <>
+      <article>
+        <div>
+          <h1>Title</h1>
+          <p>Description</p>
+        </div>
+        <button>Delete</button>
+      </article>
+    </>
+  );
+};
+
+export default CourseGoal;

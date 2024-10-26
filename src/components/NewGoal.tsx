01486987948b1afb7df26bd45931c261290363cf
TanmayKolase <tanmay.kolase@p99soft.com>
2024-10-26 17:37:10 +0530

    Implemented a newGoal component with form init

---
+++ src/components/NewGoal.tsx
@@ -0,0 +1,26 @@
+import { FormEvent } from "react";
+
+const NewGoal = () => {
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <p>
+          <label htmlFor="goal">Your goal</label>
+          <input id="goal" type="text" />
+        </p>
+        <p>
+          <label htmlFor="summary">Short summary</label>
+          <input id="summary" type="text" />
+        </p>
+        <p>
+          <button type="submit">Add Goal</button>
+        </p>
+      </form>
+    </>
+  );
+};
+
+export default NewGoal;

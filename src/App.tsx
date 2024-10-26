df7db0b8647d9e5bde4ace4fdd6a043a10b1f169
TanmayKolase <tanmay.kolase@p99soft.com>
2024-10-26 16:07:41 +0530

    Added CourseGoal interface , State to handle the addGoals status and handlers for it

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,45 @@
 import CourseGoal from "./components/CourseGoal";
 import Header from "./components/Header";
 import goalsImg from "./assets/goals.jpg";
+import { useState } from "react";
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  id: number;
+}
 
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function handleAddGoal() {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: "Learn React + TS",
+        description: "Learn it in depth",
+      };
+
+      return [...prevGoals, newGoal];
+    });
+  }
+
   return (
     <main>
       <Header image={{ src: goalsImg, alt: "A list of goals" }}>
         <h1>Your Course Goals</h1>
       </Header>
 
-      <CourseGoal title="Learn React + TS">
-        <p>Learn it from the ground up</p>
-      </CourseGoal>
+      <button onClick={handleAddGoal}>Add Goal</button>
+      <ul>
+        {goals.map((goals) => (
+          <li key={goals.id}>
+            <CourseGoal title={goals.title}>
+              <p>{goals.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }

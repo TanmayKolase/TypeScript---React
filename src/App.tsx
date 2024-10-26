59012d6fea0d407de81def58b663c0b5232e9ba7
TanmayKolase <tanmay.kolase@p99soft.com>
2024-10-26 17:36:44 +0530

    Added a newGoal component in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import goalsImg from "./assets/goals.jpg";
 import { useState } from "react";
 import CourseGoalList from "./components/CourseGoalList";
+import NewGoal from "./components/NewGoal";
 
 export interface CourseGoal {
   title: string;
@@ -35,7 +36,7 @@ export default function App() {
         <h1>Your Course Goals</h1>
       </Header>
 
-      <button onClick={handleAddGoal}>Add Goal</button>
+      <NewGoal />
       <CourseGoalList goals={goals} onDeleteGoal={handleDeleteGoal} />
     </main>
   );

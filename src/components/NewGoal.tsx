b4cd0a3b970514b1a99ed8259c6bcc4b8ae91c2f
TanmayKolase <tanmay.kolase@p99soft.com>
2024-10-26 18:09:50 +0530

    Implemented the handleSubmit func and accept user input values using useRef

--- src/components/NewGoal.tsx
+++ src/components/NewGoal.tsx
@@ -1,19 +1,31 @@
-import { FormEvent } from "react";
+import { FormEvent, useRef } from "react";
 
-const NewGoal = () => {
-  function handleSubmit(event: FormEvent) {
+interface NewGoalProps {
+  onAddGoal: (goal: string, summary: string) => void;
+}
+const NewGoal = ({ onAddGoal }: NewGoalProps) => {
+  const goal = useRef<HTMLInputElement>(null);
+  const summary = useRef<HTMLInputElement>(null);
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
+
+    const enteredGoal = goal.current!.value;
+    const enteredSummary = summary.current!.value;
+
+    event.currentTarget.reset();
+    onAddGoal(enteredGoal, enteredSummary);
   }
   return (
     <>
       <form onSubmit={handleSubmit}>
         <p>
           <label htmlFor="goal">Your goal</label>
-          <input id="goal" type="text" />
+          <input id="goal" type="text" ref={goal} />
         </p>
         <p>
           <label htmlFor="summary">Short summary</label>
-          <input id="summary" type="text" />
+          <input id="summary" type="text" ref={summary} />
         </p>
         <p>
           <button type="submit">Add Goal</button>

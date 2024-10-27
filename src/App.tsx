eab7075b19b0badd1573835a5d8956f27b23c322
TanmayKolase <tanmay.kolase@p99soft.com>
2024-10-27 22:21:31 +0530

    Initial setup & add component folder under src & created input file in it

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,5 @@
-import CourseGoal from "./components/CourseGoal";
-import Header from "./components/Header";
-import goalsImg from "./assets/goals.jpg";
-import { useState } from "react";
-import CourseGoalList from "./components/CourseGoalList";
-import NewGoal from "./components/NewGoal";
-
-export interface CourseGoal {
-  title: string;
-  description: string;
-  id: number;
+function App() {
+  return <h1>Let's get started!</h1>;
 }
 
-export default function App() {
-  const [goals, setGoals] = useState<CourseGoal[]>([]);
-
-  function handleAddGoal(goal: string, summary: string) {
-    setGoals((prevGoals) => {
-      const newGoal: CourseGoal = {
-        id: Math.random(),
-        title: goal,
-        description: summary,
-      };
-
-      return [...prevGoals, newGoal];
-    });
-  }
-
-  function handleDeleteGoal(id: number) {
-    setGoals((prevGoals) => prevGoals.filter((goal) => goal.id !== id));
-  }
-
-  return (
-    <main>
-      <Header image={{ src: goalsImg, alt: "A list of goals" }}>
-        <h1>Your Course Goals</h1>
-      </Header>
-
-      <NewGoal onAddGoal={handleAddGoal} />
-      <CourseGoalList goals={goals} onDeleteGoal={handleDeleteGoal} />
-    </main>
-  );
-}
+export default App;
